4eda91af84d7d4af61896a1f287e92d1ff7eeddd
Joyce Soares <joycespn6@gmail.com>
2021-08-04 19:27:25 -0300

    update implementation

--- Demo-dao-jdbc/src/application/Program.java
+++ Demo-dao-jdbc/src/application/Program.java
@@ -35,10 +35,17 @@ public class Program {
 
         //inserindo no banco
         System.out.println("\n=== TEST 4 seller insert ===");
-        //java.sql.Date date = new java.sql.Date();
         Seller newSeller = new Seller(null, "Rafael", "rafael@gmail", new Date(), 4000.00, dp);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        //fazendo update no banco
+        System.out.println("\n=== TEST 5 seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Rodrigo");
+        sellerDao.update(seller);
+        System.out.println("Update Completed!!");
+
 
     }
 }
